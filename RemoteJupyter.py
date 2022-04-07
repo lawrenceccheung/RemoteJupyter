05cbb475cadee3ccbf1c49538b3a1c0ebb166a46
Lawrence Cheung <15526007+lawrenceccheung@users.noreply.github.com>
2022-04-06 23:30:43 -0700

    Added ssh function

--- RemoteJupyter.py
+++ RemoteJupyter.py
@@ -9,6 +9,41 @@ sys.path.insert(1, scriptpath)
 import tkyamlgui as tkyg
 import argparse
 import subprocess, shlex
+import getpass
+import pexpect
+import tempfile
+
+def ssh(host, cmd, user, password, timeout=30, inputopts='',bg_run=False):
+    """SSH'es to a host using the supplied credentials and executes a
+    command.  Throws an exception if the command doesn't return 0.
+    bgrun: run command in the background
+    """
+    # From 
+
+    fname = tempfile.mktemp()
+    fout = open(fname, 'w')
+    
+    options = inputopts+' -q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oPubkeyAuthentication=no'
+    if bg_run:
+        options += ' -f'
+    ssh_cmd = 'ssh %s@%s %s "%s"' % (user, host, options, cmd)
+    child = pexpect.spawn(ssh_cmd, timeout=timeout)  #spawnu for Python 3
+    child.expect(['[pP]assword: '])
+    child.sendline(password)
+
+    child.logfile = fout
+    child.expect(pexpect.EOF)
+    child.close()
+    fout.close()
+    
+    fin = open(fname, 'r')
+    stdout = fin.read()
+    fin.close()
+    
+    if 0 != child.exitstatus:
+        raise Exception(stdout)
+    
+    return stdout
 
 class MyApp(tkyg.App, object):
     def __init__(self, *args, **kwargs):
@@ -24,11 +59,10 @@ class MyApp(tkyg.App, object):
         user      = self.inputvars['username'].getval()
         machine   = self.inputvars['servername'].getval()
         execmd    = servercmd.format(NBLAB=NBLAB, REMOTEPORT=REMOTEPORT)
-        sshcmd    = "ssh {user}@{machine} "
-        exestring = sshcmd.format(user=user, machine=machine, execmd=execmd)
-        print(shlex.split(exestring))
 
-        subprocess.call(exestring, shell=True)
+        pwd = getpass.getpass()
+        out=ssh(machine,'w',user,pwd)
+        print(out)
 
         return
 
